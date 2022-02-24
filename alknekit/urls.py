02cca68b28e2ea556aec82c7cd7823f7346992cf
lukasvici <izi98izi2@gmail.com>
2022-02-24 19:11:52 +0400

    url catalog/category_id & correct vue

--- alknekit/urls.py
+++ alknekit/urls.py
@@ -4,4 +4,5 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/<int:category_id>/', views.list_products, name='catalog'),
 ]
--- alknekit/views.py
+++ alknekit/views.py
@@ -2,12 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-from .models import Products
+from .models import Products, Category
 
 
 def index(request):
-    obj = Products.objects.filter(main=3)[:0]
+    obj = Products.objects.filter(main=3)[:4]
+    category = Category.objects.filter()
     context = {
+        "cat":category,
         'obj':obj,
     }
-    return  render(request, "../templates/alknekit/products.html", context)
+    return render(request, "../templates/alknekit/index.html", context)
+
+def list_products(request, category_id):
+    obj = Products.objects.filter(subcategory=category_id)[:20]
+    context = {
+        'obj': obj,
+    }
+    return render(request, "../templates/alknekit/catalog.html", context)
